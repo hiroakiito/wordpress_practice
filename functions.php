fe8c7878f823b0c29c9a0b214dbfad7225b974d3
hiroakiito <pigaru.gooood.mar19@gmail.com>
2020-05-01 23:17:43 +0900

    pc記事本文以外の大枠作成

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
+<?php
 function my_styles() {
-  wp_enqueue_style( 'style', get_template_directory_uri() . 'style.css', array(), '1.0.3' );
+  wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0.3' );
 }
 add_action( 'wp_enqueue_scripts', 'my_styles' );
--- index.php
+++ index.php
@@ -5,9 +5,129 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel=”SHORTCUT ICON” href=”/image/logo.png” />
-  <title>バズ部</title>
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+  <title>センスがグッとアップする。そのまま使える英語メール書き出し80選</title>
+  <?php wp_head(); ?>
 </head>
 <body>
-    <h1>テスト</h1>
+
+<!-- ヘッダー -->
+<header class="header">
+    <div class="container">
+        <div class="head-left">
+            <a href="#">Berlitz blog</a>
+            <p>世界で活躍したいを叶える</p>
+        </div>
+        
+        <div class="head-right">
+            <p class="top-logo">
+                <img src="<?php bloginfo('template_directory'); ?>/images/logo_vertical.png">
+            </p>
+        </div>
+    </div>
+</header>
+
+<!-- ナビゲーションメニュー -->
+ <nav class="nav-menu">
+    <ul class="container">
+        <li>
+            <span>Buisiness</span>
+            <a href="">ビジネス英語を話す</a>
+        </li>
+        <li>
+            <span>Buisiness</span>
+            <a href="">ビジネス英語を書く</a>
+        </li>
+        <li>
+            <span>Buisiness</span>
+            <a href="">便利なビジネス英語まとめ</a>
+        </li>
+        <li>
+            <span>Daily Conversation</span>
+            <a href="">日常英会話</a>
+        </li>
+        <li>
+            <span>Traning</span>
+            <a href="">英会話トレーニング</a>
+        </li>
+        <li>
+            <span>Other Language</span>
+            <a href="">諸外国語</a>
+        </li>
+    </ul>
+</nav>
+
+<!-- メインコンテンツ -->
+<div class="main-container">
+    <div class="container">
+        <ul>
+            <li><a href=""><i class="fa fa-home"></i>  HOME</a></li>/
+            <li><a href=""><i class="fa fa-folder"></i>  ビジネス英語を書く</a></li>/
+            <li><a href=""><i class="far fa-file"></i>  センスがグッとアップする。そのまま使える英語メール書き出し80選</a></li>
+        </ul>
+    </div>
+    <div class="content container">
+        <!-- 記事本文 -->
+        <main>
+
+        </main>
+        <!-- サイドバー -->
+        <aside>
+            <div class="sns-container">
+                <p>ソーシャルメディアで購読出来ます</p>
+                <ul>
+                    <li><a href=""><i class="fab fa-facebook-square fa-2x"></i>Facebookでフォロー</a></li>
+                    <li><a href=""><i class="fab fa-twitter fa-2x"></i>Twitterでフォロー</a></li>
+                    <li><a href=""><i class="fas fa-rss-square fa-2x"></i>Feedlyでフォロー</a></li>
+                </ul>
+            </div>
+            <div class="ranking-container">
+                <p>人気のある記事</p>
+                <ul>
+                    <li ><a href="">相手やシーンにあわせて感謝を英語で伝える「ありがとう」50選</a></li>
+                    <li><a href="">センスがグッとアップする。そのまま使える英語メール書き出し80選</a></li>
+                    <li><a href="">ネイティブが使う！英語で「久しぶり」シチュエーション別10選</a></li>
+                    <li><a href="">今さら聞けない英語の日付！書き方・読み方、曜日等</a></li>
+                    <li><a href="">英語で数字の単位が読み方まで一覧でわかる！音声付きまとめ</a></li>
+                    <li><a href="">クリスマスメッセージやカードに使いたい英語の例文48撰</a></li>
+                    <li><a href="">英語で新年の挨拶！気のきいたフレーズ等50選</a></li>
+                    <li><a href="">絶対に覚えたい！シチュエーション別の英語挨拶30フレーズ</a></li>
+                    <li><a href="">ネイティブがスラっと使う英語の「どういたしまして」シーン別10選</a></li>
+                    <li><a href="">もう怖くない英語の道案内。カンタンよくある20フレーズ</a></li>
+                </ul>
+            </div>
+        </aside>
+    </div>
+</div>
+
+<div class="blog-link">
+    <a href="#">Berlitz blog</a>
+</div>
+
+<div class="footer-menu">
+    <ul class="container">
+        <li><a href="">ビジネス英語を話す</a></li>
+        <li><a href="">ビジネス英語を書く</a></li>
+        <li><a href="">便利なビジネス英語まとめ</a></li>
+        <li><a href="">日常英会話</a></li>
+        <li><a href="">英会話トレーニング</a></li>
+        <li><a href="">諸外国語</a></li>
+    </ul>
+</div>
+
+
+<footer class="footer">
+    <div class="container">
+        <div class="foot-left">
+            <a href="#">個人情報の取り扱い</a>
+            <a href="#">ご利用上の注意</a>
+        </div>
+        
+        <div class="foot-right">
+        <small><copy></copy>©2020 Berlitz Japan, Inc.</small>
+        </div>
+    </div>
+</footer>
+    <?php wp_footer(); ?>
 </body>
 </html>
